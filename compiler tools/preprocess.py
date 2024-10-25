7e9635332818ab220db7b220f2852aad48a26bc1
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-10-25 22:53:42 +0330

    preprocess changed for multi librarires

--- compiler tools/preprocess.py
+++ compiler tools/preprocess.py
@@ -4,6 +4,7 @@ this will process our code and get needed parts for compile.
 
 import re
 import os
+import json
 
 class PreprocessError(Exception):
     def __init__(self, message):
@@ -19,18 +20,26 @@ class Preprocessor:
 
 
     def read_libraries(self, path):
-        file_txt = open(path, 'r').read()
-        match = re.search(r'#include <(.*)>', file_txt)
+        file_txt = open(path, 'r').read().split('\n')
+        print(file_txt)
+        match = [find[0] for find in [re.findall(r'^\s*#include\s*<(.*)>\s*', line) for line in file_txt] if find]
         if match:
-            lib_name = re.search(r'(.*)\..*',match.group(1))
-            if not lib_name:
+            lib_names = [re.findall(r'(.*)\..*', name)[0] for name in match]
+            if not lib_names:
                 lib_name = match.group(1)
             if not os.path.isfile(os.path.join('..', 'libraries', lib_name, '.json')):
                 raise PreprocessError(f'{lib_name} doesn\'t exist in libraries')
             else:
                 with (open(os.path.join('..', 'libraries', lib_name, '.json'), 'r') as lk,
                       open('data/coreKeywords.json') as ck):
-                    pass
+                    l_keywords = json.load(lk)
+                    c_keywords = json.load(ck)
+
+
+if __name__ == '__main__':
+    p = Preprocessor()
+    p.read_libraries(os.path.join('..', 'testfiles', 'valid', 'all.c'))
+
 
 
 
--- testfiles/valid/all.c
+++ testfiles/valid/all.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-
+//#include <stdio.h>
+#include <stdlib.h>
+#include <ali.h>
 
 char valid(){
     string itisvalid = 4;
