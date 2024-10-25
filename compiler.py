cef689dff91765476edb03b95446db8e53b0f644
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-10-25 20:25:09 +0330

    lexical funtion added to the compiler class

--- compiler.py
+++ compiler.py
@@ -13,13 +13,34 @@ class CompilerErorr(Exception):
 class Compiler:
     def __init__(self, file_path):
         Compiler.is_exist(file_path)
-        self.file_path = file_path
-
+        self.__SYMBOL_TABLE = {
+            'Code': list(),
+            'Type': list(),
+            'Token': list()
+        }
+        self.source_code = self.file_read(file_path)
 
     @staticmethod
     def is_exist(file_path):
         if not os.path.exists(file_path):
             raise CompilerErorr('file doesn\'t exist, please recheck path address')
+
+    def file_read(self, file_path):
+        with open(file_path, 'r') as f:
+            return f.read()
+
+    def __lexical_analysis(self):
+        """
+        main function to analyze lexically and send to next phase.
+        :return: result of analyze
+        """
+        pass
+
+
+
+
+
+
 
 
 
