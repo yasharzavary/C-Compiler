bf7120f4102fa4677cda3f781159e514585f3a91
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2024-10-25 21:26:23 +0330

    need library installed, preprocess mathc libary added

--- compiler tools/preprocess.py
+++ compiler tools/preprocess.py
@@ -2,13 +2,27 @@
 this will process our code and get needed parts for compile.
 """
 
+import re
+import os
+
+class PreprocessError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+
 class Preprocessor:
     def __init__(self):
         pass
 
 
-    def read_libraries(self,path):
+    def read_libraries(self, path):
         file_txt = open(path, 'r').read()
-        # TODO: preprocess
+        match = re.search(r'#include <(.*)>', file_txt)
+        if match:
+            pass
+
 
 
